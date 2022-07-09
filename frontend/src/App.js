b5d9e07082a145119e4de0e5aa2bb1ca169b9cfe
izimio <joshua.brionne@gmail.com>
2022-07-09 16:28:31 +0200

    [ADD] patern for beg of the front with fetching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 function App() {
+  let [data, setData] = useState(() => 0)
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/url/`
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        let actualData = await response.json();
+        console.log(actualData)
+        setData(JSON.stringify(actualData));
+      } catch(err) {
+        setData("ddd");
+      }
+    }
+    getData()
+  }, [])
+
   return (
-   <>
-   <p>siiu</p>
-   </>
+    <>
+      <p>{data}</p>
+    </>
   );
 }
 
